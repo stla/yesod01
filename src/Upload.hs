511651d69e0e864ebddc3e660bf356c8b4d81917
stla <laurent_step@yahoo.fr>
2023-11-29 11:29:42 +0100

    meta tag

--- src/Upload.hs
+++ src/Upload.hs
@@ -34,6 +34,7 @@ getUploadR :: Handler Html
 getUploadR = defaultLayout $ do
     toWidgetHead [hamlet|
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
     |]
     setTitle "Upload"
     addStylesheetRemote "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
